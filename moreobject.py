853099729c8bffb9a703351376bf780c805ee494
Lam Ngo <127332375+wulingfy@users.noreply.github.com>
2025-01-22 17:00:54 +0700

    Update moreobject.py

--- moreobject.py
+++ moreobject.py
@@ -1,13 +1,13 @@
 import cv2
 import face_recognition 
 
-img_org = cv2.imread(r'omage')
+img_org = cv2.imread(r'path_img')
 
 rgb_org = cv2.cvtColor(img_org, cv2.COLOR_BGR2RGB)
 
 wife_code = face_recognition.face_encodings(rgb_org)[0]
 pass_wife = [wife_code]
-class_img = cv2.imread(r'imageimage')
+class_img = cv2.imread(r'path_img')
 class_img= cv2.resize(class_img,(1200,700))
 class_rbg = cv2.cvtColor(class_img, cv2.COLOR_BGR2RGB)
 face_position = face_recognition.face_locations(class_rbg)
@@ -26,5 +26,5 @@ for (top,right,bottom,left),key in zip(face_position,face_passcode):
         class_img = cv2.rectangle(class_img,(left,top),(right,bottom),color,thick)
         class_img = cv2.putText(class_img,'??',(left-10,top-20),font,1,(255,0,0),2)
    
-cv2.imshow('find wife',class_img)
+cv2.imshow('detect',class_img)
 cv2.waitKey(0)
